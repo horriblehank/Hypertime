ab2c97ada28314feb3cf5bcfc445e4d8baecb32c
Henrik <leaftwig@hotmail.com>
2019-06-14 14:34:05 +0200

    Changed URL

--- hypertime-e2e/test/e2e/hypertime.js
+++ hypertime-e2e/test/e2e/hypertime.js
@@ -3,14 +3,18 @@ const ENTER = "\uE007"
 module.exports = { // adapted from: https://git.io/vodU0
   'Guinea Pig Assert Title': function(browser) {
     browser
-      .url('https://hypertimehenrik.z16.web.core.windows.net/')
+      .url('http://henriklofqvist.com/ajax')
       .waitForElementVisible('body')
-      .assert.title('Hypertime')
-      .setValue("#username", "horriblehank")
-      .keys(ENTER)
-      .saveScreenshot('hypertime.png')
-      //.pause(10000)
-      .end()
-      .pause();
+      .assert.title('Stanley Cup winners')
+      //.setValue("#username", "horriblehank")
+      //.keys(ENTER)
+      //.saveScreenshot('hypertime.png')
+      .assert.containsText("#placeholder > div > h1", "Choose a season above and find out which team won the Stanley Cup that year!")
+      
+      .click("#y2012")
+      .pause(2000)
+      .click("#y2000")
+      .pause()
+      .end();
   }
 };
